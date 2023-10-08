6eb3e18945f6d942de42634b0a8612f411c335f7
Souvik Dey <92887905+sdcode001@users.noreply.github.com>
2023-10-09 02:51:56 +0530

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 
 
@@ -13,8 +14,20 @@ class InputModel(BaseModel):
 
 
 app = FastAPI()
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
 pickle_in = open("trained_model.pkl", "rb")
 predictor = pickle.load(pickle_in)
+
 
 @app.get('/test')
 def fun():
@@ -25,12 +38,12 @@ def fun():
 def fun(data: InputModel):
     data = data.dict()
     prediction = predictor.predict([[data['WindDirection'], data['month'], data['day'], data['Hour'], data['meanSpeed']]])
-    return {'prediction': prediction[0]}
+    return {'output_power_prediction': prediction[0]}
 
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=5000)
 
 
 #uvicorn main_api:app --host 0.0.0.0 --port 10000
